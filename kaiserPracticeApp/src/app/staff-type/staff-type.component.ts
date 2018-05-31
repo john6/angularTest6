7157eb0573192e42b44fab5c9df69e65cbf476ab
John Russell (john.p.russell) <john.p.russell@avanade.com>
2018-05-30 17:05:47 -0700

    added basic skeleton

---
+++ kaiserPracticeApp/src/app/staff-type/staff-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-type',
+  templateUrl: './staff-type.component.html',
+  styleUrls: ['./staff-type.component.css']
+})
+export class StaffTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
